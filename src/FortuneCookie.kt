362972de628bc5c5adde17912ad7fc5fbeeb3344
Manoj M <31803525+mano-j@users.noreply.github.com>
2019-01-15 20:43:41 +0530

    Lesson 3: Exercise 7. Extra practice.

--- src/FortuneCookie.kt
+++ src/FortuneCookie.kt
@@ -1,16 +1,25 @@
-fun main(args: Array<String>){
-    print(getFortuneCookie())
-}
+fun main(args: Array<String>) {
+    for(counter in 1..10) {
+        val fortune = getFortuneCookie()
+        println("Your fortune is : $fortune \n")
 
+        // Loops for 10 times or until the fortune is "Take it easy and enjoy life!".
+        if(fortune.startsWith("Take")) break
+    }
+}
 fun getFortuneCookie() : String{
-    val fortunes = listOf("You will have a great day!", "Things will go well for you today.", "Enjoy a wonderful day of success.",
-        "Be humble and all will turn out well.", "Today is a good day for exercising restraint.", "Take it easy and enjoy life!",
+    val fortunes = listOf("You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
         "Treasure your friends because they are your greatest fortune.")
 
-    println("Enter your Birthday: ")
-    val birthday = readLine()!!.toIntOrNull()
-    val index: Int = birthday?.rem(fortunes.size) ?: -1
+        println("Enter your Birthday: ")
+        val birthday = readLine()!!.toIntOrNull()
+        val index = birthday?.rem(fortunes.size) ?: -1
 
-    // Notice return statement can be lifted out of if..else/when. Since if..else/when can be used as expressions in Kotlin.
-    return if(index != -1) fortunes[index] else "Enter a valid Day"
+        // Notice return statement can be lifted out of if..else/when. Since if..else/when can be used as expressions in Kotlin.
+        return if(index != -1) fortunes[index] else "\"Enter a valid Day\""
 }
