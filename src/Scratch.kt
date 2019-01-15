e2262ff12095ac19492fceb469aea6cc330568da
Manoj M <31803525+mano-j@users.noreply.github.com>
2019-01-15 12:37:07 +0530

    Passing arguments to main function. (Greetings, Kotlin)

--- src/Scratch.kt
+++ src/Scratch.kt
@@ -1,18 +1,3 @@
-import java.util.*
-
 fun main(args: Array<String>){
-    dayOfWeek()
-}
-
-fun dayOfWeek(){
-    print("What day is today? : ")
-    when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
-        1 -> print("Sunday")
-        2 -> print("Monday")
-        3 -> print("Tuesday ")
-        4 -> print("Wednesday")
-        5 -> print("Thursday")
-        6 -> print("Friday")
-        7 -> print("Saturday")
-    }
+    println("Good ${if (args[0].toInt() < 12) "morning" else "night"}, Kotlin. ")
 }
