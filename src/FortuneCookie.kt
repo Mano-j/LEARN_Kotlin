0ef1dd29bb499ebfe32fd3d09933017e91c069e8
Manoj M <31803525+mano-j@users.noreply.github.com>
2019-01-15 19:32:33 +0530

    Lesson 3: Exercise 7. Added FortuneCookie.kt

---
+++ src/FortuneCookie.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+    print(getFortuneCookie())
+}
+
+fun getFortuneCookie() : String{
+    val fortunes = listOf("You will have a great day!", "Things will go well for you today.", "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.", "Today is a good day for exercising restraint.", "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune.")
+
+    println("Enter your Birthday: ")
+    val birthday = readLine()!!.toIntOrNull()
+    val index: Int = birthday?.rem(fortunes.size) ?: -1
+
+    return if(index != -1) fortunes[index] else "Enter a valid Day"
+}
