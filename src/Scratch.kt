a171b39e6b9a0373fa8367a14ed9db7172bb72bb
Manoj M <31803525+mano-j@users.noreply.github.com>
2019-01-17 03:04:21 +0530

    Lesson 3: Exercise 19. Kotlin Lambdas.

--- src/Scratch.kt
+++ src/Scratch.kt
@@ -1,3 +1,25 @@
+import java.util.*
+
 fun main(args: Array<String>){
     println("Good ${if (args[0].toInt() < 12) "morning" else "night"}, Kotlin. ")
+
+    val rollDice = { sides: Int ->
+        if(sides != 0 ) Random().nextInt(sides) + 1
+        else 0
+    }
+
+    val rollDice1: (Int) -> Int = { sides ->
+        if (sides == 0) 0
+        else Random().nextInt(sides) + 1
+    }
+
+//    println(rollDice(2))
+//    println(rollDice1(4))
+    gamePlay(rollDice1(6))
+    gamePlay(rollDice1(6))
+    gamePlay(rollDice1(6))
+}
+
+fun gamePlay(operation: Int){
+    println(operation)
 }
